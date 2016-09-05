9d8a2dc6d34eed07796e47366eb4530ffa41e9e7
Geraldo Braho <gbraho@na.edu>
2016-09-05 03:57:49 -0500

    changed name

---
+++ Algorithms/Assignment1.py
@@ -0,0 +1,8 @@
+#Write a program that finds minimum, maximum and average of a list.
+
+
+list=[12,32,2,4,67,1,2,6,7,8,9,10,15,14,19]
+list.sort()
+print max(list)
+print min(list)
+print sum(list)/2
