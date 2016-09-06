40c2bca0219b00c95286fa9b8f35cac59acc3d66
Geraldo Braho <gbraho@na.edu>
2016-09-05 23:46:22 -0500

    Update gcd.py

--- Algorithms/gcd.py
+++ Algorithms/gcd.py
@@ -1,18 +1,31 @@
-import random
-import matplotlib.pyplot as plt
-average = 0.0
-def GCD(a, b):
-    average=float((a+b)/2)
-    count=0
-    while (b != 0):
-        remainder  = a % b
-        a = b
-        b = remainder
-        count+=1
-    return count
 
-gcd_values = GCD(random.randint(1,10000),random.randint(1,10000))
-print "It takes " +str(gcd_values) +  " steps to find the GCD"
-gcdSteps = plt.bar(1,gcd_values, width=0.3,color='c',label="gcd steps")
-averageSteps = plt.bar(2,1,width=0.3,color='m',label="average")
-plt.show()
+def gcd(a, b)
+    while (b!= 0):
+       reminder= b
+       b = a% b
+       a = reminder 
+    return a
+    
+    
+    
+    
+    
+#another way
+def gcd(a, b)
+    while (a!=b):
+        if a > b
+           a = a − b; 
+        else
+           b = b − a; 
+    return a
+    
+    
+#    another way
+def gcd(a, b)
+    if b = 0
+       return a
+    else
+       return gcd(b, a % b)
+
+
+
