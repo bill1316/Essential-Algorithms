48dcb392b663e2cb536dbc629129f1dc8d144f10
Geraldo Braho <gbraho@na.edu>
2016-10-03 14:52:21 -0500

    Create BubbleSort.py

---
+++ Algorithms/Sorting Algorithms/BubbleSort.py
@@ -0,0 +1,17 @@
+///Bubble  Sort Algorithms.
+//Author: Geraldo Braho
+//date:10/03/2016
+
+
+
+def bubbleSort(A):
+    for i in range(0,len(A)-1):
+        for j in range(0,len(A)-1-i):
+            if A[j]>A[j+1]:
+                A[j],A[j+1]=A[j+1],A[j]
+
+    return A
+
+
+my_list=['b','f','c','a','e','m']
+print (bubbleSort(my_list))
