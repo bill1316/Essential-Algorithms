bb9689b4ad844adde1c5afb33279abffc158345f
Geraldo Braho <gbraho@na.edu>
2016-10-04 23:25:56 -0500

    Update Insertion Sort Algorithms.py

--- Algorithms/Sorting Algorithms/Insertion Sort Algorithms.py
+++ Algorithms/Sorting Algorithms/Insertion Sort Algorithms.py
@@ -10,7 +10,7 @@ It runs O(n**2)'''
 
 #1.Using for loop to sort
 def insertion_sort(A):
-    for in range(1,len(A)):
+    for i in range(1,len(A)):
         for j in range(i-1,0,-1):
             if A[j]>A[j+1]:
                 A[j],A[j+1]=A[j+1],A[j]
